eb00f11b6bd6bdfb977528f625a5f3619b2a22a4
soorya sree k <sooryasreekmanoj@gmail.com>
2024-07-14 10:15:13 +0530

    feat: added email notification under profile settings

---
+++ app/profile/notifications/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import  {EmailNotifications } from '../_componets/email-notifications' 
+
+/*
+  Author: sooryasree & Ayisha rafa  on July 2nd, 2024
+*/
+
+const Notification = () => {
+  return (
+    <div>
+     <EmailNotifications />
+    </div>
+  )
+}
+export default Notification
