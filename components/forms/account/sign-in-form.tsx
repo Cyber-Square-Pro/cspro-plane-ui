9fd657c8280bec31928cc9c9068eaf6b60ff84f4
Tysha Daniels <2023158020@ub.edu.bz>
2026-04-18 03:36:27 -0600

    feat(ui): implement card component and refactor sign-in UI

--- components/forms/account/sign-in-form.tsx
+++ components/forms/account/sign-in-form.tsx
@@ -28,35 +28,32 @@ export const SignInForm: React.FC<Props> = (props) => {
 
   return (
     <form onSubmit={handleSubmit(onFormSubmit)}>
-      <div className="py-2">
-         
-        <Input
-          className="w-full border rounded-md"
-          placeholder="Enter your email"
-          {...register("email")}
-        />
-      </div>
+      <div className="space-y-4">
+        <div>
+          <Input
+            className="w-full border rounded-md"
+            placeholder="Enter your email"
+            {...register("email")}
+          />
+        </div>
 
-      <div className="py-2">
-        <Input
-          className="w-full border rounded-md"
-          placeholder="Enter your password"
-          type="password"
-          {...register("password")}
-        />
-      </div>
-      <div className="py-2">
-        <Button
-          className="w-full border rounded-md"
-          disabled={!isValid}
-          type="submit"
-        >
-          Login
-        </Button>
-      </div>
-      <div className="py-2 text-center">
-        <span className="bg-slate-50"> Dont have an account?</span>
-        <Link href="/sign-up"> Signup</Link>
+        <div>
+          <Input
+            className="w-full border rounded-md"
+            placeholder="Enter your password"
+            type="password"
+            {...register("password")}
+          />
+        </div>
+        <div>
+          <Button
+            className="w-full border rounded-md"
+            disabled={!isValid}
+            type="submit"
+          >
+            Login
+          </Button>
+        </div>
       </div>
     </form>
   );
